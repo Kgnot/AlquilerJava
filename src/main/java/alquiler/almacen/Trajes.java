2b0f92f2d3448b7c71fd77cfea85e361d714d0ed
Henry Ricaurte Mora <hricaurtem@proton.me>
2024-09-23 12:49:53 -0500

    Test

---
+++ src/main/java/alquiler/almacen/Trajes.java
@@ -0,0 +1,24 @@
+package alquiler.almacen;
+
+import alquiler.traje.Traje;
+
+import java.util.ArrayList;
+
+public class Trajes {
+    private static ArrayList<Traje> trajes;
+    private static Trajes instanciaT;
+
+    private Trajes(){}
+
+    public static Trajes getInstancia(){
+        if(instanciaT == null){
+            instanciaT = new Trajes();
+        }
+        return instanciaT;
+    }
+    public ArrayList<Traje> getListaTrajes(){
+        return trajes;
+    }
+
+
+}
